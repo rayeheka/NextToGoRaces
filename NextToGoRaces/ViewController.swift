ada4e51487ea93cc3eb61fd28845a5738d9a17d1
Rayeheh Karimanipour <rayeheh.karimanipour@snapp.cab>
2024-08-25 23:42:42 +1000

    Initial Commit

---
+++ NextToGoRaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NextToGoRaces
+//
+//  Created by Rayeheh Karimanipour on 8/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
