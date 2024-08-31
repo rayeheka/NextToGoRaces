1feab7dedf374dea3b2d1acae07c7045bcb52553
Rayeheh Karimanipour <rayeheh.karimanipour@snapp.cab>
2024-08-31 12:25:19 +1000

    add conversion func to convert raw seconds to a better format to show

--- NextToGoRaces/ViewModels/RaceSummaryViewModel.swift
+++ NextToGoRaces/ViewModels/RaceSummaryViewModel.swift
@@ -13,6 +13,9 @@ class RaceSummaryViewModel: ObservableObject {
     var category: RaceCategory
     var advertisedStartDate: Date
     var advertisedStart: Double
+    var formattedAdvertisedStart: String {
+        convertSecondsToFormattedTime(seconds: advertisedStart)
+    }
     
     init(raceSummary: RaceSummary) {
         self.meetingName = raceSummary.meetingName
@@ -20,5 +23,22 @@ class RaceSummaryViewModel: ObservableObject {
         self.category = raceSummary.category
         self.advertisedStartDate = raceSummary.advertisedStart
         self.advertisedStart = raceSummary.advertisedStart.timeIntervalSinceNow
+    }
+    
+    /// convert raw seconds to formatted h min sec
+    ///
+    /// - Parameters:
+    ///     - seconds: A Double that represents seconds to be converted
+    /// - Returns: a String in the format of "h min sec"
+    private func convertSecondsToFormattedTime(seconds: Double) -> String {
+        let intSeconds = Int(seconds)
+        let h = intSeconds / 3600
+        let min = (intSeconds % 3600) / 60
+        let sec = (intSeconds % 3600) % 60
+        var convertedString = ""
+        if h != 0 { convertedString.append("\(h) h ") }
+        if min != 0 { convertedString.append("\(min) m ") }
+        convertedString.append("\(sec) s")
+        return convertedString
     }
 }
